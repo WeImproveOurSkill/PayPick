dca02621eb593d15081785a706318423d025e434
benidene <dlgusehd09@naver.com>
2025-06-09 03:24:03 +0900

    <fe> <refactor> : Change modal status management store to useRerenderStore, add detailStore

--- fe/src/store/modal.ts
+++ fe/src/store/modal.ts
@@ -1,15 +1,37 @@
 import {create} from "zustand";
 
-interface ModalState {
-  show: boolean;
-  storeModal: (state:boolean) => void;
+interface detailState {
+  detailShow: boolean;
+  detailModal: (state:boolean) => void;
+}
+
+export const useDetailStore = create<detailState>((set) => ({
+  detailShow: false,
+  detailModal: (state) => set(() => ({ detailShow: state })),
+}));
+
+
+interface rerenderState {
   rerenderShow: boolean;
   rerenderModal: (state:boolean) => void;
 }
 
-export const useModalStore = create<ModalState>((set) => ({
-  show: false,
-  storeModal: (state) => set(() => ({ show: state })),
+export const useRerenderStore = create<rerenderState>((set) => ({
   rerenderShow: false,
   rerenderModal: (state) => set(() => ({ rerenderShow: state })),
+}));
+
+
+interface recentlyStoreState {
+  recentlyShow: boolean;
+  isClosing: boolean;
+  recentlyModal: (state: boolean) => void;
+  setClosing: (state: boolean) => void;
+}
+
+export const useRecentlyStore = create<recentlyStoreState>((set) => ({
+  recentlyShow: false,
+  isClosing: false,
+  recentlyModal: (state) => set(() => ({ recentlyShow: state })),
+  setClosing: (state) => set(() => ({ isClosing: state })),
 }));
