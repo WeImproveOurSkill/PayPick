0db653c0c6c786107ccfa9e8373ab0f583594bea
benidene <dlgusehd09@naver.com>
2025-06-09 03:25:36 +0900

    <fe> <style> : Modify chat page CSS - Adjust padding values, add scrollbar style, improve button style

--- fe/src/app/chat/[storeId]/page.css.ts
+++ fe/src/app/chat/[storeId]/page.css.ts
@@ -6,7 +6,7 @@ export const container = style({
   flexDirection: 'column',
   width: '100dvw',
   height: 'calc(100dvh - 40px)',
-  padding: '72px 20px 20px 20px',
+  padding: '82px 30px 30px 30px',
   backgroundColor: 'rgb(248, 249, 253)',
   justifyContent: 'center',
  });
@@ -14,7 +14,7 @@ export const container = style({
  export const chatBorder = style({
   display: 'flex',
   flexDirection: 'column',
-  overflowY: 'scroll', 
+  // overflowY: 'scroll', 
   height: '100%', 
   border: '1px solid lightGray', 
   padding: '10px',
@@ -29,6 +29,21 @@ export const container = style({
   wordWrap: 'break-word', // 단어가 길면 줄바꿈
   wordBreak: 'break-all', // 긴 단어는 중간에 끊어서라도 줄바꿈
   whiteSpace: 'pre-wrap', // 여러 줄 입력을 허용하고, 공백도 유지
+  '::-webkit-scrollbar': {
+    width: '10px'
+  },
+  '::-webkit-scrollbar-track': {
+    background: 'transparent'
+  },
+  '::-webkit-scrollbar-thumb': {
+    background: 'gray',  // 밝은 회색으로 설정
+    borderRadius: '5px',
+    border: '2px solid transparent',
+    backgroundClip: 'padding-box'  // border를 제외한 영역에만 배경색 적용
+  },
+  // Firefox를 위한 스크롤바 스타일
+  scrollbarWidth: 'thin',
+  scrollbarColor: 'gray transparent'
  });
 
  export const messageContainer = style({
@@ -42,7 +57,6 @@ export const container = style({
 export const messageBox = style({
   display: 'inline-block',
   maxWidth: '100%',
-  minWidth: '20%',
   padding: '10px 15px',
   borderRadius: '20px',
   wordWrap: 'break-word',
@@ -79,8 +93,44 @@ export const time = style({
  });
 
  export const button = style({
-  width: '20px', 
-  height:'30px',
   background: 'none',
- });
+  alignItems: 'center',
+  selectors: {
+    '&:active': {
+      background: 'none'
+    }
+  }
+});
+
+export const sendIcon = style({
+  width: '30px',
+  height: '30px'
+});
+
+// 버튼 active 상태일 때 이미지 스타일 변경
+globalStyle(`${button}:active ${sendIcon}`, {
+  filter: 'brightness(0.8)'
+});
+
+//  export const button = style({
+//   width: '30px', 
+//   height:'30px',
+//   background: 'none',
+//   selectors: {
+//     '&:active': {
+//       background: 'none'
+//     }
+//   }
+//  });
+
+//  export const sendIcon = style({
+//   width: '30px',
+//   height: '30px',
+//   position: 'absolute',
+//   left: 0,
+//   top: '50%',
+//   transform: 'translateY(-50%)'
+// });
+
+
 
