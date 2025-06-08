db3f2e48bc5c937cca15badef6bd003fc6347ae1
benidene <dlgusehd09@naver.com>
2025-06-09 03:23:08 +0900

    <fe> <refactor> : Recently added shop functionality and implemented modal functionality

--- fe/src/app/_component/FooterMenu.tsx
+++ fe/src/app/_component/FooterMenu.tsx
@@ -1,19 +1,36 @@
-import Link from 'next/link'
+"use client"
+
 import * as styles from './FooterMenu.css'
+import Link from 'next/link'
+import { useRecentlyStore } from "@/store/modal"
+import RecentlyViewedStores from './RecentlyViewedStores'
+
 
 const FooterMenu = () => {
+
+  const { recentlyShow, recentlyModal, setClosing } = useRecentlyStore();
+
+  const handleClick = () => {
+    if (recentlyShow) {
+      setClosing(true);
+      setTimeout(() => {
+        setClosing(false);
+        recentlyModal(false);
+      }, 300);
+    } else {
+      recentlyModal(true);
+    }
+  };
+
   return (
-    <ul className={styles.container}>
-      <li className={styles.menu}>
-        <Link href='/'>최근 본 상점</Link>
-      </li>
-      <li className={styles.borderMenu}>
-        <Link href='/'>홈</Link>
-      </li>
-      <li className={styles.menu}>
-       <Link href='/report'>문의</Link>
-      </li>
-    </ul>
+    <div className={styles.container}>
+      <div className={styles.menu} onClick={handleClick}>
+        최근 본 상점
+      </div>
+      <RecentlyViewedStores recentlyShow={recentlyShow}/>
+      <Link className={styles.borderMenu} href='/'>홈</Link>
+      <Link className={styles.menu} href='/report'>문의</Link>
+    </div>
   )
 }
 
